fdf2b0e3d4e32c9b96d87de39cc0d6f589c19830
rafadevx <rafa.comp@gmail.com>
2019-10-06 13:57:14 -0300

    implemented subtotal and total

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -7,11 +7,13 @@ import {
   MdDelete,
 } from 'react-icons/md';
 
+import { formatPrice } from '../../util/format';
+
 import * as CartActions from '../../store/modules/cart/actions';
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmount }) {
   function increment(product) {
     updateAmount(product.id, product.amount + 1);
   }
@@ -54,7 +56,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
                 </div>
               </td>
               <td>
-                <strong>R$258,90</strong>
+                <strong>{product.subtotal}</strong>
               </td>
               <td>
                 <button
@@ -74,7 +76,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
 
         <Total>
           <span>TOTAL</span>
-          <strong>R$1920,28</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
@@ -82,7 +84,15 @@ function Cart({ cart, removeFromCart, updateAmount }) {
 }
 
 const mapStateToProps = state => ({
-  cart: state.cart,
+  cart: state.cart.map(prod => ({
+    ...prod,
+    subtotal: formatPrice(prod.price * prod.amount),
+  })),
+  total: formatPrice(
+    state.cart.reduce((total, prod) => {
+      return total + prod.price * prod.amount;
+    }, 0)
+  ),
 });
 
 const mapDispatchToProps = dispatch =>
